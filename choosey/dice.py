b47aa1240a3dc8e18c23dde1f00314208cd3d7ad
Noah Hoffman <noah.hoffman@gmail.com>
2022-02-12 15:52:49 -0800

    simplify dice art

--- choosey/dice.py
+++ choosey/dice.py
@@ -1,4 +1,7 @@
-# dice.py
+"""
+Adapted from https://realpython.com/python-dice-roll/
+"""
+
 import random
 
 DICE_ART = {
@@ -45,54 +48,42 @@ DICE_ART = {
         "└─────────┘",
     ),
 }
-DIE_HEIGHT = len(DICE_ART[1])
-DIE_WIDTH = len(DICE_ART[1][0])
-DIE_FACE_SEPARATOR = " "
 
 
-# dice.py
-
-# ...
-
-def draw_dice(dice_values):
+def draw_dice(dice_values, dice_art=DICE_ART):
     """Return an ASCII diagram of dice faces from `dice_values`.
 
     The string returned contains an ASCII representation of each die.
     For example, if `dice_values = [4, 1, 3, 2]` then the string
     returned looks like this:
 
-    ~~~~~~~~~~~~~~~~~~~ RESULTS ~~~~~~~~~~~~~~~~~~~
     ┌─────────┐ ┌─────────┐ ┌─────────┐ ┌─────────┐
     │  ●   ●  │ │         │ │  ●      │ │  ●      │
     │         │ │    ●    │ │    ●    │ │         │
     │  ●   ●  │ │         │ │      ●  │ │      ●  │
     └─────────┘ └─────────┘ └─────────┘ └─────────┘
     """
-    # Generate a list of dice faces from DICE_ART
-    dice_faces = []
-    for value in dice_values:
-        dice_faces.append(DICE_ART[value])
 
-    # Generate a list containing the dice faces rows
-    dice_faces_rows = []
-    for row_idx in range(DIE_HEIGHT):
-        row_components = []
-        for die in dice_faces:
-            row_components.append(die[row_idx])
-        row_string = DIE_FACE_SEPARATOR.join(row_components)
-        dice_faces_rows.append(row_string)
+    # get a list of dice drawings (each item in dice will be a list of lines)
+    dice = [dice_art[val] for val in dice_values]
+    nrows = len(dice[0])
 
-    # Generate header with the word "RESULTS" centered
-    width = len(dice_faces_rows[0])
-    diagram_header = " RESULTS ".center(width, "~")
+    lines = []
+    # for each line in the die drawings...
+    for row in range(nrows):
+        # collect one line from each die drawing and connect them, separated by
+        # a space
+        lines.append(' '.join(die[row] for die in dice))
 
-    dice_faces_diagram = "\n".join([diagram_header] + dice_faces_rows)
-    return dice_faces_diagram
+    # join all of the lines together into a single string
+    return '\n'.join(lines)
+
 
 def roll_amount(times):
     roll = []
     for i in range(times):
         roll.append(random.randint(1, 6))
     return roll
+
 
 
