fb2d4415cb43deff7dee0ab6ee22c4bc03d9e195
Noah Hoffman <noah.hoffman@gmail.com>
2022-02-12 16:52:37 -0800

    add missing edges to graph

--- choosey/parse_game.py
+++ choosey/parse_game.py
@@ -2,6 +2,8 @@ import sys
 import configparser
 import pprint
 import textwrap
+import random
+import string
 
 try:
     from . import actions
@@ -22,13 +24,25 @@ def graph(game, label_width=12):
     for step, d in game.items():
         fromname = nodename(step)
         nodelabel = graphlabel(step, width=label_width)
+        if '_action' in d:
+            nodelabel += '\n[{}]'.format(d['_action'])
+
         lines.append(f'{fromname} [ label="{nodelabel}" ];')
+        # if one of the options is '???', create a dummy node to connect to
+        if '???' in d.values():
+            dummy = ''.join(random.choices(string.ascii_letters, k=6))
+            lines.append(f'{dummy} [ label="???" ];')
+
         for key, val in d.items():
+            edgelabel = graphlabel(key, width=label_width)
             if val in game:
                 toname = nodename(val)
-                edgelabel = graphlabel(key, width=label_width)
                 lines.append(f'"{fromname}" -> "{toname}" [ label="{edgelabel}" '
-                             f'color=grey fontcolor=grey];')
+                             'color=grey fontcolor=grey];')
+            elif val == '???':
+                lines.append(f'"{fromname}" -> "{dummy}" [ label="{edgelabel}" '
+                             'color=grey fontcolor=grey];')
+
 
     lines.append('}\n')
     return '\n'.join(lines)
